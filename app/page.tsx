14eb5c22bce233f1ebf3114583cc65a4a3352910
Nana Mohamadi <adilnana78@gmail.com>
2025-10-27 00:39:59 +0000

    React Client & Server Components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Hello from "./component/hello";
+
 const Home = () => {
   return (
     <div className="text-2xl underline flex justify-center items-center min-h-screen">
       Welcome to Nextjs
+      <Hello />
     </div>
   );
 };
